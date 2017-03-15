3821e8747f3531ff499d2865e18c645e9e46882e
Tom Goetz <tom.goetz@virtualcomputer.com>
2017-03-15 18:01:06 -0400

    improved activity summary stats

--- garmin_connect_export.py
+++ garmin_connect_export.py
@@ -20,6 +20,14 @@ class GarminDataXlsx():
         self.worksheet = None
 
 
+    def next_row(self):
+        self.row += 1
+
+
+    def column_letter(self, index):
+        return chr(ord('A') + index)
+
+
     def calculate_fit(self, string):
         length = len(string) + self.autofit_col_padding
         if self.col >= self.col_count:
@@ -31,6 +39,7 @@ class GarminDataXlsx():
 
     def calculate_date_fit(self):
         self.calculate_fit(self.date_format_str)
+
 
     def auto_fit(self):
         for index, col_width in enumerate(self.col_widths):
@@ -56,6 +65,102 @@ class GarminDataXlsx():
         self.col += 1
 
 
+    def write_cell_heading(self, heading):
+        self.write_cell_string(heading, self.heading_format)
+
+
+    def write_average_row(self, row_start, row_end, avg_cols):
+        self.col = 0
+        self.write_cell_string("Average", self.heading_format)
+        for avg_col in avg_cols:
+            col_letter = self.column_letter(avg_col)
+            formula = "=AVERAGE(%c%d:%c%d)" % (col_letter, row_start + 1, col_letter, row_end + 1)
+            logging.debug("Average %d : %s" % (avg_col, formula))
+            self.worksheet.write_formula(self.row, avg_col, formula)
+        self.row += 1
+
+
+    def write_conditional_avg_row(self, row_start, row_end, conditional_col, condition, col):
+        self.col = 0
+        self.write_cell_string("Average %s" % condition, self.heading_format)
+        cond_col_letter = self.column_letter(conditional_col)
+        col_letter = self.column_letter(col)
+        formula = '=AVERAGEIF(%c%d:%c%d, "%s", %c%d:%c%d)' % \
+            (cond_col_letter, row_start + 1, cond_col_letter, row_end + 1, condition, total_col_letter, row_start + 1, col_letter, row_end + 1)
+        logging.debug("AverageCond %d : %s" % (col, formula))
+        self.worksheet.write_formula(self.row, col, formula)
+        self.row += 1
+
+
+    def write_total_row(self, row_start, row_end, total_cols):
+        self.col = 0
+        self.write_cell_string("Total", self.heading_format)
+        for total_col in total_cols:
+            col_letter = self.column_letter(total_col)
+            formula = "=SUM(%c%d:%c%d)" % (col_letter, row_start + 1, col_letter, row_end + 1)
+            logging.debug("Total %d : %s" % (total_col, formula))
+            self.worksheet.write_formula(self.row, total_col, formula)
+        self.row += 1
+
+
+    def write_conditional_total_row(self, row_start, row_end, conditional_col, condition, total_col):
+        self.col = 0
+        self.write_cell_string("Total %s" % condition, self.heading_format)
+        cond_col_letter = self.column_letter(conditional_col)
+        total_col_letter = self.column_letter(total_col)
+        formula = '=SUMIF(%c%d:%c%d, "%s", %c%d:%c%d)' % \
+            (cond_col_letter, row_start + 1, cond_col_letter, row_end + 1, condition, total_col_letter, row_start + 1, total_col_letter, row_end + 1)
+        logging.debug("TotalCond %d : %s" % (total_col, formula))
+        self.worksheet.write_formula(self.row, total_col, formula)
+        self.row += 1
+
+
+    def write_max_row(self, row_start, row_end, max_cols):
+        self.col = 0
+        self.write_cell_string("Max", self.heading_format)
+        for max_col in max_cols:
+            col_letter = self.column_letter(max_col)
+            formula = "=MAX(%c%d:%c%d)" % (col_letter, row_start + 1, col_letter, row_end + 1)
+            logging.debug("Max %d : %s" % (max_col, formula))
+            self.worksheet.write_formula(self.row, max_col, formula)
+        self.row += 1
+
+
+    def write_conditional_max_row(self, row_start, row_end, conditional_col, condition, max_col):
+        self.col = 0
+        self.write_cell_string("Max %s" % condition, self.heading_format)
+        cond_col_letter = self.column_letter(conditional_col)
+        max_col_letter = self.column_letter(max_col)
+        formula = '{=MAX(IF(%c%d:%c%d="%s",%c%d:%c%d))}' % \
+            (cond_col_letter, row_start + 1, cond_col_letter, row_end + 1, condition, max_col_letter, row_start + 1, max_col_letter, row_end +1)
+        logging.debug("MaxCond %d : %s" % (max_col, formula))
+        self.worksheet.write_formula(self.row, max_col, formula)
+        self.row += 1
+
+
+    def write_min_row(self, row_start, row_end, min_cols):
+        self.col = 0
+        self.write_cell_string("Min", self.heading_format)
+        for min_col in min_cols:
+            col_letter = self.column_letter(min_col)
+            formula = "=MIN(%c%d:%c%d)" % (col_letter, row_start + 1, col_letter, row_end + 1)
+            logging.debug("Min %d : %s" % (min_col, formula))
+            self.worksheet.write_formula(self.row, min_col, formula)
+        self.row += 1
+
+
+    def write_conditional_min_row(self, row_start, row_end, conditional_col, condition, min_col):
+        self.col = 0
+        self.write_cell_string("Min %s" % condition, self.heading_format)
+        cond_col_letter = self.column_letter(conditional_col)
+        min_col_letter = self.column_letter(min_col)
+        formula = '{=MIN(IF(%c%d:%c%d="%s",%c%d:%c%d))}' % \
+            (cond_col_letter, row_start + 1, cond_col_letter, row_end + 1, condition, min_col_letter, row_start + 1, min_col_letter, row_end +1)
+        logging.debug("MinCond %d : %s" % (min_col, formula))
+        self.worksheet.write_formula(self.row, min_col, formula)
+        self.row += 1
+
+
     def start_activity(self, activity_name):
         logging.info("Writing activity '%s'..." % activity_name)
         self.row = 0
@@ -63,13 +168,11 @@ class GarminDataXlsx():
         self.worksheet = self.workbook.add_worksheet(activity_name)
 
 
-    def write_headings(self, start_col, headings):
+    def write_headings(self, headings, start_col=0):
         self.row = 0
         self.col = start_col
         for heading in headings:
-            self.worksheet.write_string(self.row, self.col, heading, self.heading_format)
-            self.calculate_fit(heading)
-            self.col += 1
+            self.write_cell_heading(heading)
         self.row = 1
         self.col = 0
 
@@ -88,7 +191,8 @@ class GarminDataXlsx():
         self.row += 1
         for value_name in values_dict:
             logging.debug("Footer %s : %d" % (value_name, values_dict[value_name]))
-            self.worksheet.write_string(self.row, 0, value_name, self.heading_format)
+            self.col = 0
+            self.write_cell_heading(value_name)
             self.worksheet.write_number(self.row, 1, values_dict[value_name])
             self.row += 1
 
@@ -130,6 +234,30 @@ class GarminActivityData():
         self.last_date = None
         self.days = 0
         self.lines = []
+
+
+    def activity_attr(self, activity_type, attr_name):
+        activity_types = {
+                'Active Calories' : { 'total_col' : [2], 'total_cond' : [ 1, 2, [ 'Walking', 'Hiking', 'Running' ] ],
+                    'min_cond' : [ 1, 2, [ 'Walking', 'Hiking', 'Running' ] ], 'max_cond' : [ 1, 2, [ 'Walking', 'Hiking', 'Running' ] ] },
+                'Average Heart Rate' : { 'avg_cols' : [1] },
+                'Calories Consumed' : { 'avg_cols' : [1], 'total_col' : [1], 'max_cols' : [1], 'min_cols' : [1] },
+                'Floors Climbed' : { 'avg_cols'  : [1, 3], 'total_col' : [1, 3], 'max_cols' : [1, 3], 'min_cols' : [1, 3] },
+                'Intensity Minutes' : { 'avg_cols'  : [1], 'total_col' : [1], 'max_cols' : [1], 'min_cols' : [1] },
+                'Max Heart Rate' : { 'avg_cols'  : [1], 'total_col' : [], 'max_cols' : [1], 'min_cols' : [1] },
+                'Resting Heart Rate' : { 'avg_cols'  : [1], 'total_col' : [], 'max_cols' : [1], 'min_cols' : [1] },
+                'Sleep Time' : { 'avg_cols'  : [1], 'total_col' : [1] },
+                'Steps' : { 'avg_cols'  : [1], 'total_col' : [1] },
+                'Total Calories' : { 'total_col' : [2], 'total_cond' : [ 1, 2, [ 'Active', 'BMR' ] ], 'min_cond' : [ 1, 2, [ 'Active', 'BMR' ] ],
+                    'max_cond' : [ 1, 2, [ 'Active', 'BMR' ] ] },
+                'Total Distance' : { 'total_col' : [2], 'total_cond' : [ 1, 2, [ 'Walking', 'Hiking', 'Running' ] ],
+                    'min_cond' : [ 1, 2, [ 'Walking', 'Hiking', 'Running' ] ], 'max_cond' : [ 1, 2, [ 'Walking', 'Hiking', 'Running' ] ] },
+            }
+        try:
+            attr = (activity_types[activity_type])[attr_name]
+        except:
+            attr = None
+        return (attr)
 
 
     def statistics(self):
@@ -259,17 +387,50 @@ class GarminActivityData():
             self.import_file(input_file)
 
 
+    def write_summary(self, function, activity_type, attr_name):
+        cols = self.activity_attr(self.title, attr_name)
+        if cols:
+            function(1, self.record_count, cols)
+
+
+    def write_conditional_summary(self, function, activity_type, condition_name):
+        conditions = self.activity_attr(activity_type, condition_name)
+        if conditions:
+            conditional_col = conditions[0]
+            col = conditions[1]
+            condition_strings = conditions[2]
+            for condition_string in condition_strings:
+                function(1, self.record_count, conditional_col, condition_string, col)
+            return True
+        return False
+
+
     def write_file(self, gd_xlsx):
         gd_xlsx.start_activity(self.title)
 
         # write the column headings
-        gd_xlsx.write_headings(0, self.headings)
+        gd_xlsx.write_headings(self.headings)
 
         self.lines.sort()
 
         # write the data
         for values in self.lines:
             gd_xlsx.write_activity_row(values)
+
+        gd_xlsx.next_row()
+        self.write_summary(gd_xlsx.write_min_row, self.title, 'min_cols')
+        self.write_summary(gd_xlsx.write_max_row, self.title, 'max_cols')
+        self.write_summary(gd_xlsx.write_average_row, self.title, 'avg_cols')
+        self.write_summary(gd_xlsx.write_total_row, self.title, 'total_col')
+
+        gd_xlsx.next_row()
+        if self.write_conditional_summary(gd_xlsx.write_conditional_min_row, self.title, 'min_cond'):
+            gd_xlsx.next_row()
+        if self.write_conditional_summary(gd_xlsx.write_conditional_max_row, self.title, 'max_cond'):
+            gd_xlsx.next_row()
+        if self.write_conditional_summary(gd_xlsx.write_conditional_avg_row, self.title, 'avg_cond'):
+            gd_xlsx.next_row()
+        self.write_conditional_summary(gd_xlsx.write_conditional_total_row, self.title, 'total_cond')
 
         gd_xlsx.write_activity_footer({"Files" : self.file_count, "Records" : self.record_count, "Days" : self.days})
         gd_xlsx.auto_fit()
@@ -319,7 +480,7 @@ class GarminData():
  
         for index, activity_type in enumerate(self.stats):
             if index == 0:
-                gd_xlsx.write_headings(1, self.stats[activity_type])
+                gd_xlsx.write_headings(self.stats[activity_type], 1)
             gd_xlsx.write_stats_row(activity_type, self.stats[activity_type])
 
         gd_xlsx.auto_fit()
